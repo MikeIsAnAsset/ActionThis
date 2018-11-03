99588394ddb1946624a36cb8ed3ab8ab8bd5f77c
Mike Win <mikeIsAnAsset@gmail.com>
2018-11-03 14:26:22 -0500

    Initial Commit

---
+++ src/main/java/com/example/ActionThis/ActionThisApplication.java
@@ -0,0 +1,12 @@
+package com.example.ActionThis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionThisApplication.class, args);
+	}
+}
